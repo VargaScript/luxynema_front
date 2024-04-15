c43a52c812487b56299c93ee8180c3bd5c5db6ab
JhoanaC <abicor339@gmail.com>
2024-04-15 16:35:33 -0700

    membership

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Login } from './components/Login/Login';
 // import { Payment } from './components/Payment/Payment';
 // import { Schedule } from './components/Schedule/Schedule';
 // import { AboutUs } from './components/AboutUs/AboutUs';
-// import { Membership } from './components/Membership/Membership';
+
 // import { AllMovies } from './components/AllMovies/AllMovies';
 // import { Account } from './components/Account/Account';
 // import { ErrorPage } from './components/ErrorPage/ErrorPage';
@@ -14,8 +14,8 @@ import { AboutUs } from './components/Client/AboutUs/AboutUs';
 import { AllMovies } from './components/Client/AllMovies/AllMovies';
 import { Schedule } from './components/Client/Schedule/Schedule';
 import { Payment } from './components/Client/Payment/Payment';
-import { Membership } from './components/Client/Membership/Membership';
 import { Account } from './components/Client/Account/Account';
+import Membership from './components/Client/Membership/Membership';
 
 const App = () => {
   return (
@@ -26,7 +26,7 @@ const App = () => {
       <Route path="/home" element={<Home />} /> 
       {/* <Route path="/schedule" element={<Schedule />} /> */}
       {/* <Route path="/about-us" element={<AboutUs />} /> */}
-      {/* <Route path="/membership" element={<Membership />} /> */}
+      <Route path="/membership" element={<Membership />} /> 
       {/* <Route path="/all-movies" element={<AllMovies />} /> */}
       {/* <Route path="/account" element={<Account />} /> */}
       {/* <Route path="/payment" element={<Payment />} /> */}
